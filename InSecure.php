31ae33347878cbf6992362519aa04617712e9346
Jeff Armstrong <jarmstrong@nezasoft.com>
2019-01-17 15:09:37 -0800

    Add files via upload

---
+++ InSecure.php
@@ -0,0 +1,20 @@
+<?php
+
+// First a SQL Injection attack
+$var = $_POST['var'];
+mysql_query("SELECT * FROM sometable WHERE id = $var");
+
+//
+/// XSS example
+//
+$var = $_POST['var'];
+echo "<div>$var</div>\n";
+
+//
+/// Forget to terminate user input after a redirect
+//
+if ($_SESSION['user_logged_in'] !== true) {
+  header('Location: /login.php');
+}
+
+//omg_important_private_functionality_here();
