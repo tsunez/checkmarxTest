b7f31202b9580e7493751b52bc9d49292b0b92a7
Jeff Armstrong <jarmstrong@nezasoft.com>
2020-10-31 10:28:51 -0700

    Update InSecure.php

--- InSecure.php
+++ InSecure.php
@@ -1,7 +1,7 @@
 <?php
 
 
-// Updated to trigger CxFlow 373
+// Updated to trigger CxFlow 385
 // First a SQL Injection attack V9
 //$var = $_POST['var'];
 //mysql_query("SELECT * FROM sometable WHERE id = $var");
