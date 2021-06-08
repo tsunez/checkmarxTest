e8863c979080ee489c11fdf42a33de348606f822
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-06-08 08:51:21 -0700

    Update InSecure.php

--- InSecure.php
+++ InSecure.php
@@ -14,18 +14,4 @@
 $var = $_POST['var'];
 echo "<div>$var</div>\n";
 
-
-//
-/// 2nd XSS example
-//
-$var = $_POST['varB'];
-echo "<div>$varB</div>\n";
-
-//
-/// Forget to terminate user input after a redirect
-//
-if ($_SESSION['user_logged_in'] !== true) {
-  header('Location: /login.php');
-}
-
 // Important private logic that shouldn't happen because we've already redirected the user!
