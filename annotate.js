f7dd1d96e6d04fe593491d0f6627264ce495f12d
root <root@DESKTOP-3G1P0QQ.localdomain>
2021-10-19 21:03:38 -0700

    Fix

--- annotate.js
+++ annotate.js
@@ -5,17 +5,24 @@ const require = createRequire(import.meta.url);
 
 //const core = require('@actions/core');
 //const Github = require('@actions/github');
-const Github = require('@/github');
+const github = require('@actions/github');
 
 console.log("Adding AST Result Annotations");
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 console.log("Token: " + GITHUB_TOKEN)
-//const octokit = github.getOctokit(GITHUB_TOKEN);
+const octokit = github.getOctokit(GITHUB_TOKEN);
 console.log("Got octokit")
 
-const github = new Github(GITHUB_TOKEN)
-await github.createCheck("AST Annotations")
+//const github = new Github(GITHUB_TOKEN)
+await octokit.createCheck("AST Annotations")
 
+const result = await octokit.request('POST /repos/{owner}/{repo}/check-runs', {
+    owner: 'tsunez',
+    repo: 'checkmarxTest',
+    name: 'name',
+    head_sha: 'head_sha'
+});
+console.log(result)
 
 //const { context = {} } = github;
 //const { pull_request } = context.payload;
