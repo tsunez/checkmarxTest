f438ef389ce5be76ef6232c7dedbd7c5e0195658
root <root@DESKTOP-3G1P0QQ.localdomain>
2021-10-19 21:18:15 -0700

    Fix

--- annotate.js
+++ annotate.js
@@ -5,17 +5,17 @@ const require = createRequire(import.meta.url);
 
 //const core = require('@actions/core');
 //const Github = require('@actions/github');
-const github = require('@actions/github');
+//const github = require('@actions/github');
 
 console.log("Adding AST Result Annotations");
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-console.log("Token: " + GITHUB_TOKEN)
-const octokit = github.getOctokit(GITHUB_TOKEN);
-console.log("Got octokit")
+//const octokit = github.getOctokit(GITHUB_TOKEN);
+
 
 //const github = new Github(GITHUB_TOKEN)
 //await octokit.createCheck("AST Annotations")
 // octokit.c
+/*
 const result = await octokit.request('POST /repos/{owner}/{repo}/check-runs', {
     owner: 'tsunez',
     repo: 'checkmarxTest',
@@ -23,6 +23,38 @@ const result = await octokit.request('POST /repos/{owner}/{repo}/check-runs', {
     head_sha: 'head_sha'
 });
 console.log(result)
+*/
+
+const { Octokit } = require('@octokit/rest');
+const octokit = new Octokit({
+    auth: GITHUB_TOKEN,
+});
+
+const owner = 'tsunez';
+const repo = 'checkmarxTest';
+author = {
+    name: 'Jeff Armstrong',
+    email: 'jarmstrong@nezasoft.com',
+};
+const url =  '/repos/{owner}/{repo}/{path}'; 
+const ref =  'heads/master';
+
+
+// Test Code
+const getContents = async () => {
+    const { data } = await octokit.request({
+        owner,
+        repo,
+        url,
+        method: 'GET',
+        path: 'contents', // gets the whole repo
+    });
+    console.log(data)
+}
+getContents();
+
+
+
 
 //const { context = {} } = github;
 //const { pull_request } = context.payload;
