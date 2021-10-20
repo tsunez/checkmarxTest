b469c291465513ad5e0de51d8cfc60d4d8e2bd05
root <root@DESKTOP-3G1P0QQ.localdomain>
2021-10-19 19:06:14 -0700

    Added initial test to interact with GitHub environment.

---
+++ src/annotate.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+console.log("Adding AST Result Annotations");
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
+const octokit = github.getOctokit(GITHUB_TOKEN);
+
+const { context = {} } = github;
+const { pull_request } = context.payload;
+
+// Create comment
+await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
+});
+
+console.log("Token: " + GITHUB_TOKEN)
+
+console.log("Done creating AST tokens")
