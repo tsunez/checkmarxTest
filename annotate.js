257369252012ad39a615dd2759b07f63e207eb7c
root <root@DESKTOP-3G1P0QQ.localdomain>
2021-10-19 20:41:30 -0700

    Fix

--- annotate.js
+++ annotate.js
@@ -15,12 +15,16 @@ console.log("Got octokit")
 const { context = {} } = github;
 const { pull_request } = context.payload;
 
+console.log(octokit)
 // Create comment
+//octokit.issues.createComment()
+/*
 await octokit.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
     body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
 });
+*/
 
 console.log("PtsToken: " + GITHUB_TOKEN)
 
