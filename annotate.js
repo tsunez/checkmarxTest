d1e48a0053712b8a7ccefb69bcaa8fa190752f99
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-10-21 21:11:49 -0700

    Fix

--- annotate.js
+++ annotate.js
@@ -2,79 +2,38 @@
 //const require = global.require || global.process.mainModule._load;
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-
-
-//const core = require('@actions/core');
-//const Github = require('@actions/github');
-//const github = require('@actions/github');
-
-console.log("Adding AST Result Annotations");
-const GH_TOKEN = process.env.GH_TOKEN;
-const APP_KEY = process.env.APP_KEY;
-const APP_SECRET = process.env.APP_SECRET;
-
-
-//const octokit = github.getOctokit(GITHUB_TOKEN);
-
-
-//console.log("token: " + GH_TOKEN)
-
-//const github = new Github(GITHUB_TOKEN)
-//await octokit.createCheck("AST Annotations")
-// octokit.c
-/*
-const result = await octokit.request('POST /repos/{owner}/{repo}/check-runs', {
-    owner: 'tsunez',
-    repo: 'checkmarxTest',
-    name: 'name',
-    head_sha: 'head_sha'
-});
-console.log(result)
-*/
-
 const { Octokit } = require('@octokit/rest');
 const { createAppAuth } = require("@octokit/auth-app");
 
-// Token based auth
+console.log("Adding AST Result Annotations");
+// Removed these
+//const GH_TOKEN = process.env.GH_TOKEN;
+
+const APP_GH_KEY = process.env.APP_GH_KEY;
+const GITHUB_RUN_ID = process.env.GITHUB_RUN_ID;
+//const APP_ID = process.env.APP_ID;
+//const APP_INSTALL_ID = process.env.APP_INSTALL_ID;
+//const APP_CLIENT_ID = process.env.APP_ClIENT_ID;
+//const APP_SECRET = process.env.APP_SECRET;
+//const APP_KEY = process.env.APP_KEY;
+
+console.log("RUN ID: " + GITHUB_RUN_ID);
+
+// Token based auth, if tokens are supported
+/*
 const octokit = new Octokit({
     auth: GH_TOKEN,
 });
-
-// App based auth
-/*
-const appOctokit = new Octokit({
-    authStrategy: createAppAuth,
-    auth: {
-      appId: 1,
-      privateKey: APP_KEY,
-      clientId: APP_ID
-    },
-});
 */
 
-const auth = createAppAuth({
-    appId: "146135",
-    privateKey: APP_KEY,
-    installationId: 20198899,
-    clientId: 'Iv1.dc2272c7e822d613',
-    clientSecret: APP_SECRET,
-});   
-const { token } = await auth({ type: "app" });
-//const { token } = await auth({ type: "installation" });
-console.log(`Git repo token: ` + token);
+const octokit = new Octokit({
+});
 
-
+const url =  '/repos/{owner}/{repo}/{path}'; 
 const owner = 'tsunez';
 const repo = 'checkmarxTest';
-const author = {
-    name: 'Jeff Armstrong',
-    email: 'jarmstrong@nezasoft.com',
-};
-const url =  '/repos/{owner}/{repo}/{path}'; 
 const ref =  'heads/gh_action_test';
 
-
-// Test Code
 const createAnotations = async () => {
 
     // Locate the branch to put the annotations on
@@ -85,12 +44,14 @@ const createAnotations = async () => {
     })
     const headSha = refData.object.sha
     console.log("sha: " + headSha)
-    // Create the check run
-    await octokit.request({
+    //
+    /// Create the check run
+    //
+    /*
+    console.log("GH: " + APP_GH_KEY)
+    const result = await octokit.request({
         headers: {
-            //authentication: `token ${token}`,
-            authorization: `token ${token}`
-            //authorization: `Bearer ${token}`,
+            authorization: `token ${APP_GH_KEY}`
         },
         owner,
         repo,
@@ -98,17 +59,13 @@ const createAnotations = async () => {
         method: 'POST',
         name: 'TestCheck',
         path: 'check-runs',
-        headSha
-      })  
+        head_sha: headSha
+      })
+    console.log(result)
+    */
     console.log("Done creating check")
 }
 createAnotations()
-
-/*
-headers: {
-    authorization: `token ${localStorage.getItem('github_auth_token')}`,
-  },
-*/
 
 const getContents = async () => {
     // Create the annotation
@@ -123,14 +80,9 @@ const getContents = async () => {
     });
     console.log(data)
 }
-//getContents();
+// getContents();
 
 
-//const { context = {} } = github;
-//const { pull_request } = context.payload;
-
-//console.log(octokit.issues)
-//octokit.
 // Create comment
 //octokit.issues.createComment()
 /*
