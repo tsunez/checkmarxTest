104a7fbf2d2a855d6a3c9ed0e53e11db75caead7
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-10-24 14:02:29 -0700

    Fix

--- annotate.js
+++ annotate.js
@@ -98,9 +98,8 @@ async function createCheck(check_name, title, annotations, commitSha) {
     }
     console.log("STep 2")
     console.log(update_req)
-    await octokit.rest.checks.update(update_req).
-		  catch(error => { console.log('caught', error.message); });
-	  //]then(null, error => { console.log('caught', error.message); });
+    await octokit.checks.update(update_req).
+	catch(error => { console.log('caught', error.message); });
     console.log("DONE")
   } else {
     console.log("Didn't find check");
